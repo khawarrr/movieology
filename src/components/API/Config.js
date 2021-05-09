cbc19407c7b503192454b23812d893ac3af9582e
khawarrr <mkhawar0907@gmail.com>
2021-05-08 19:11:52 -0700

    movie poster is showing on the landing page

--- src/components/API/Config.js
+++ src/components/API/Config.js
@@ -6,3 +6,6 @@ export const API_KEY = 'ca9f756d81de0454fc2547fbc4379947';
 export const API_URL = "https://api.themoviedb.org/3/";
 // to obtain images through api
 export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
+
+
+export const IMAGE_SIZE = 'w1280';
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,19 +1,23 @@
-import { API_URL, API_KEY } from "../../components/API/Config.js"
-import { useEffect } from "react"
+import { API_URL, API_KEY, IMAGE_URL, IMAGE_SIZE } from "../../components/API/Config.js"
+import { useEffect, useState } from "react"
 import MainImage from "./Sections/MainImage.js"
-import { Typography } from 'antd';
+import { Typography, Row } from 'antd';
 
 const { Title } = Typography;
 
 
 export default function LandingPage() {
+
+  // the data we acquire from Api, we will pass it to the state
+  const [Movies, setMovies] = useState([])
   
   useEffect(() => {
     fetch(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
     .then(response => response.json())
     .then(response => {
         console.log(response)
-    })
+        setMovies(response.results)  // results come from respnse the we recieved from api
+        })
 }, [])
 
     return  (
@@ -21,28 +25,29 @@ export default function LandingPage() {
         <div style={{ width: '100%', margin: 0}} >
 
           {/* Movie Main Image  */}
-          <div style={{
-                background:
-                    `linear-gradient(to bottom, rgba(0,0,0,0)
-            39%,rgba(0,0,0,0)
-            41%,rgba(0,0,0,0.65)
-            100%),
-            url(''), #1c1c1c`,
-                height: '500px',
-                backgroundSize: '100%, cover',
-                backgroundPosition: 'center, center',
-                width: '100%',
-                position: 'relative'
-            }}>
+          <MainImage image={`${IMAGE_URL}w1280${Movies[0].backdrop_path}`} 
+          title={Movies[0].original_title} text={Movies[0].overview} />
 
-          </div>
+            {/* Body of Landing Page   */}
+            <div style={{ width: '85%', margin: '1rem auto' }}>
 
-            <div style={{ position: 'absolute', maxWidth: '500px', bottom: '2rem', marginLeft: '2rem' }}>
-            <Title style={{ color: 'white' }} level={2} > Title </Title>
-                    <p style={{ color: 'white', fontSize: '1rem' }}  > text </p>
+                <Title level={2} > Latest Movies </Title>
+                <hr /> 
 
-              
-            </div>
+
+                {/* show many movies in grid layout  */}
+
+                <Row gutter={[16, 16]} >
+
+                </Row>
+
+                <br/>
+
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <button >Load More</button>
+                </div>
+                
+                </div>
 
 
 
